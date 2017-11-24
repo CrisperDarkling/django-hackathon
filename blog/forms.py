ae799d006c9a007472d714899732d7a42518374e
Yazeed Kelly <yazeedkelly@hotmail.com>
2017-11-24 15:21:48 +0000

    7th 15:21, blogpost images working

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content', 'image')
+        fields = ('title', 'content', 'image', "published_date")
         
 class BlogCommentForm(forms.ModelForm):
     class Meta:
